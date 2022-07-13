9ef85b2846777c8c07a0ef49d1e9c2bcae953825
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-13 22:14:22 +0200

    data sets and code added

---
+++ 01_fundamentals.R
@@ -0,0 +1,21 @@
+# The Right Kind of Statistical Analysis
+# depends on:
+#   - response variable (y-axis, ordinate)
+#   - explanatory variables (x-axis, abscissa)
+
+## These simple keys will then lead you to the appropriate statistical method:
+
+# The explanatory variables:
+# a) All explanatory variables continuous: Regression
+# b) All explanatory variables categorical: Analysis of variance (ANOVA)
+# c) Some explanatory variables continuous some categorical: Analysis of covariance (ANCOVA)
+
+# The response variable:
+# a) Continuous: Regression, ANOVA or ANCOVA
+# b) Proportion: Logistic regression
+# c) Count: Log linear models
+# d) Binary: Binary logistic analysis
+# e) Time at death: Survival analysis
+
+# Error type I - reject the null hypothesis when it is TRUE
+# Error type II - accept the null hypothesis when it is FALSE 
